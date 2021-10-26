a4f70c4cc9bed69a456d265711b84f61b31d2eff
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-26 10:33:04 -0500

    Generate /api/v1/swagger.json

--- SchoolAPI/Startup.cs
+++ SchoolAPI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SchoolAPI.Data;
 using SchoolAPI.Models.Interfaces;
 using SchoolAPI.Models.Services;
@@ -15,60 +16,74 @@ using System.Threading.Tasks;
 
 namespace SchoolAPI
 {
-  public class Startup
-  {
-
-    public IConfiguration Configuration { get; }
-
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SchoolDbContext>(options =>
+            {
+                // Our DATABASE_URL from js days
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
+
+            // This map the dependency (IStudent) to the correct service (StudentService)
+            // "Whenever I see "IStudent" use "StudentService"
+            // This means that I can swap out StudentService for ANYTHING
+            // services.AddTransient<IStudent, StudentServiceViaMongo>();
+            services.AddTransient<IStudent, StudentService>();
+            services.AddTransient<ICourse, CourseService>();
+            services.AddTransient<ITechnology, TechnologyService>();
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+              options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+            services.AddSwaggerGen(options =>
+            {
+                // Make sure get the "using Statement"
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "School Demo",
+                    Version = "v1",
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger(options => {
+                options.RouteTemplate = "/api/{documentName}/swagger.json";
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+
+                endpoints.MapControllers();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<SchoolDbContext>(options =>
-      {
-        // Our DATABASE_URL from js days
-        string connectionString = Configuration.GetConnectionString("DefaultConnection");
-        options.UseSqlServer(connectionString);
-      });
-
-      // This map the dependency (IStudent) to the correct service (StudentService)
-      // "Whenever I see "IStudent" use "StudentService"
-      // This means that I can swap out StudentService for ANYTHING
-      // services.AddTransient<IStudent, StudentServiceViaMongo>();
-      services.AddTransient<IStudent, StudentService>();
-      services.AddTransient<ICourse, CourseService>();
-      services.AddTransient<ITechnology, TechnologyService>();
-
-      services.AddControllers().AddNewtonsoftJson(options =>
-        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-      );
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseRouting();
-
-      app.UseEndpoints(endpoints =>
-      {
-
-        endpoints.MapControllers();
-
-        endpoints.MapGet("/", async context =>
-              {
-                await context.Response.WriteAsync("Hello World!");
-              });
-      });
-    }
-  }
 }
