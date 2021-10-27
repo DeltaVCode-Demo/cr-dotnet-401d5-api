f2b9fb808d6d094f5f61b3bc7d117237c0eeb31f
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-27 10:39:57 -0500

    Scaffold plain Register() route

---
+++ SchoolAPI/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolAPI.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterData data)
+        {
+            return Ok();
+        }
+    }
+}
