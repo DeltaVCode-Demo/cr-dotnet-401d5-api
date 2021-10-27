ef450e1d35b03ccd02836986a8c60f7baa05073f
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-27 10:12:47 -0500

    Add ApplicationUser and inherit from IdentityDbContext<ApplicationUser>

---
+++ SchoolAPI/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAPI.Models.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column(TypeName = "DATE")] // Store in SQL as a DATE, not DATETIME
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
