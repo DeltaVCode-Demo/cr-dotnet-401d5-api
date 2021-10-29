372e3c9e642ebf9f3e97980fe595d64c24dffe5d
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-29 10:44:01 -0500

    Add HotelRoom and Hotel models

---
+++ SchoolAPI/Models/HotelRoom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAPI.Models
+{
+    public class HotelRoom
+    {
+        public int HotelId { get; set; }
+        public int RoomNumber { get; set; }
+
+        [Column(TypeName = "money")] // Default is decimal(18,2)
+        public decimal Price { get; set; }
+
+        // Navigation Property
+        public Hotel Hotel { get; set; }
+    }
+
+    public class Hotel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
